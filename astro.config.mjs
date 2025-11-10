ec02be8c30ff33d1bcb926def421ed82b64f7fee
Matt Mackenzie <mbm2228@columbia.edu>
2025-11-10 00:07:35 -0500

    add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,9 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
+
+  site: "https://mbmackenzie.github.io",
+
   integrations: [mdx(), alpinejs(), pagefind()],
 
   markdown: {
